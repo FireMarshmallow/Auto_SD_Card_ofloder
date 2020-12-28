fc1d66ac65491294fd70c1bf7c5d5c53087d816d
Mellow <62353091+FireMarshmallow@users.noreply.github.com>
2020-12-28 20:20:50 +0000

    gui

--- sd_card_off_loder.py
+++ sd_card_off_loder.py
@@ -1,8 +1,5 @@
-import csv
 import os
-import shutil
-import sys
-import time
+from shutil import move
 from time import sleep
 
 # list of paths to sd cards / drives
@@ -17,29 +14,34 @@ def path_to_drive():
 
 # cherk if drives are connected
 def if_connected():
+    # gets path from list
     for path in path_to_drive():
+        # checks if the drive is conected
         if os.path.exists(path) == True:
+            # tells the offlode def drive is conected
             offlode_card(path)
+            # passes the path
 
 
 # move files off sd card
 def offlode_card(sd):
-    # walk directory
+    # path to the directore to move files to
     move_to = "//Volumes/WinInstall/hellow"
+    # walks the directory
     for dirName, subdirList, fileList in os.walk(sd, topdown=False):
         for fname in fileList:
+            # comdins to path with the file name
             pathtofile = dirName + "/" + fname
+            # trys to move the fils or skips it
             try:
-                shutil.move(pathtofile, move_to)
+                print("moving: " + fname)
+                move(pathtofile, move_to)
             except:
                 pass
 
 
 # infenat loop
 while True:
+    print("scaning")
     if_connected()
     sleep(6)
-    print("working")
-
-
-# formeat sd?
